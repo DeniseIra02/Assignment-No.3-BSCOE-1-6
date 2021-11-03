d709d0c938aafeca2ed4a191cf37de3f2ca0a2f8
DeniseIra02 <irajubilo@gmail.com>
2021-11-03 10:36:47 +0800

    deffuntions4

--- FunctionsProg1.py
+++ FunctionsProg1.py
@@ -10,3 +10,7 @@ def getAdd():
     _add = input("Address: ")
     return _add
 
+def display(name_, age_, add_):
+    print(f"Hi, my name is {name_}. I am {age_} years old and I live in {add_}.")
+    
+
