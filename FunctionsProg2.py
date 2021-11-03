dd5afd6359b8a46c6e771a3b0fe82d6a28d056a6
DeniseIra02 <irajubilo@gmail.com>
2021-11-03 11:38:55 +0800

    Third Defining

--- FunctionsProg2.py
+++ FunctionsProg2.py
@@ -5,3 +5,9 @@ def getApl():
 def getOrng():
     _orng = int(input("How many apple do you want to buy? \n> "))
     return _orng
+
+def getPrice(apl_, orng_):
+    aplPrice = apl_ * 20
+    orngPrice = orng_ * 25
+    overallPrice = aplPrice + orngPrice
+    return overallPrice
