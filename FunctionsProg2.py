6edf2ca661b9f807e1a6add51ba170c9048705df
DeniseIra02 <irajubilo@gmail.com>
2021-11-03 11:23:55 +0800

    Second Defining

--- FunctionsProg2.py
+++ FunctionsProg2.py
@@ -1,3 +1,7 @@
 def getApl():
     _apl = int(input("How many apple do you want to buy? \n> "))
     return _apl
+
+def getOrng():
+    _orng = int(input("How many apple do you want to buy? \n> "))
+    return _orng
