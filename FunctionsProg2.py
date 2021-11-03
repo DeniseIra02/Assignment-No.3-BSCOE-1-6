bfe2bd501bd18b7abb5cf6cb4c3c151bb71f30c6
DeniseIra02 <irajubilo@gmail.com>
2021-11-03 13:02:21 +0800

    Revised Third and Last  Def

--- FunctionsProg2.py
+++ FunctionsProg2.py
@@ -6,11 +6,10 @@ def getOrng():
     _orng = int(input("How many apple do you want to buy? \n> "))
     return _orng
 
-def getPrice(apl_, orng_):
-    aplPrice = apl_ * 20
-    orngPrice = orng_ * 25
-    totalPrice = aplPrice + orngPrice
-    return totalPrice
+def getAplPrice():
+    aplPrice = 20 
+    return aplPrice
 
-def display(ovrllTotal_):
-    print(f"The total amount {ovrllTotal_} pesos.")
+def getOrngPrice():
+    orngPrice = 25 
+    return orngPrice
