12b5a272c8ef3c4b8bd2893bce60f544b0bd10e9
DeniseIra02 <irajubilo@gmail.com>
2021-11-03 11:00:34 +0800

    Last Defining

--- FunctionsProg1.py
+++ FunctionsProg1.py
@@ -9,4 +9,6 @@ def getAge():
 def getAdd():
     _add = input("Address: ")
     return _add
-    
+
+def display(name_, age_, add_):
+    print(f"Hi, my name is {name_}. I am {age_} years old and I live in {add_}.")
