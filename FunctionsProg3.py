25453a3ee937adaf3083d2b0703bcdebbbbb50f1
DeniseIra02 <irajubilo@gmail.com>
2021-11-03 14:13:52 +0800

    First and Second Def

---
+++ FunctionsProg3.py
@@ -0,0 +1,7 @@
+def getMoney():
+    _money = int(input("How much is your money? \n> "))
+    return _money
+
+def getAplPrice():
+    _aplPrice = int(input("How much is the price of apple per piece? \n> "))
+    return _aplPrice
